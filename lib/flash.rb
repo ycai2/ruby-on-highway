70c6a0d6a7a471d60fb5cb2ec2c63059cc395ea8
Yisheng Cai <cai.eason@gmail.com>
2016-09-14 10:26:38 -0400

    Put back in bracket reader and differentiate old and new cookies

--- lib/flash.rb
+++ lib/flash.rb
@@ -1,15 +1,16 @@
 require 'json'
 
 class Flash
-  HEADER_NAME = "_rails_lite_app_flash"
+  OLD_HEADER = "_rails_lite_app_flash_old"
+  NEW_HEADER = "_rails_lite_app_flash_new"
   def initialize(req)
-    @old = {JSON.parse(req.cookies[HEADER_NAME] || "{}")}
+    @old = {JSON.parse(req.cookies[OLD_HEADER] || "{}")}
     @new = {}
   end
 
-  # def [](key)
-  #   @old[key.to_s]
-  # end
+  def [](key)
+    @new[key.to_s]
+  end
 
   def []=(key, val)
     @new[key] = val
@@ -18,7 +19,8 @@ class Flash
   # serialize the hash into json and save in a cookie
   # add to the responses cookies
   def store_flash(res)
-    res.set_cookie(HEADER_NAME, @new.merge(@old).to_json)
+    res.set_cookie(OLD_HEADER, @old.to_json)
+    res.set_cookie(NEW_HEADER, @new.to_json)
   end
 
   def store_flash_now(res)
